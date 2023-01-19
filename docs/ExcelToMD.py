df04838d1cbe6d70d78076112dbda65f03dc1bad
stefanronn <114413089+stefanronn@users.noreply.github.com>
2023-01-19 13:24:24 +0100

    Add files via upload

--- docs/ExcelToMD.py
+++ docs/ExcelToMD.py
@@ -2,7 +2,6 @@
 
 import pytablereader as ptr
 import pytablewriter as ptw
-import os
 import sys
 
 
@@ -12,12 +11,10 @@ def main():
     # outfname = "C:/pythondev/ExPECA-lab-inventory-test.md"      # Output markdown file name, full path
 
     # Command line input parameters
-    argc = len(sys.argv)
- 
     # It has to be 2 command line parameters
+    argc = len(sys.argv)
     if argc != 3:
-        exit(0)
-    
+        exit(0)  
     infname = sys.argv[1]                             # Input Excel file name, full path
     outfname = sys.argv[2]                            # Output markdown file name, full path
 
@@ -30,6 +27,7 @@ def main():
             writer.from_tabledata(table_data, is_overwrite_table_name = True)
             writer.write_table()
 
+    return
 
 if __name__ == "__main__":
     main()
