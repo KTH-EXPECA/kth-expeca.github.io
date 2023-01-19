d4361960549373451c83ab4e6f13c68b4a1b5cb7
stefanronn <114413089+stefanronn@users.noreply.github.com>
2023-01-19 13:13:53 +0100

    Improved, without intermediate files

--- docs/ExcelToMD.py
+++ docs/ExcelToMD.py
@@ -9,7 +9,7 @@ import sys
 def main():
 
     # infname = "C:/pythondev/ExPECA-lab-inventory-test.xlsx"  # Input Excel file name, full path
-    # outfname = "C:/pythondev/ExPECA-lab-inventory-test"      # Output markdown file name, full path, minus the .md extension
+    # outfname = "C:/pythondev/ExPECA-lab-inventory-test.md"      # Output markdown file name, full path
 
     # Command line input parameters
     argc = len(sys.argv)
@@ -19,31 +19,16 @@ def main():
         exit(0)
     
     infname = sys.argv[1]                             # Input Excel file name, full path
-    outfname = sys.argv[2]                            # Output markdown file name, full path, minus the .md extension
-
-    status = "ok"
+    outfname = sys.argv[2]                            # Output markdown file name, full path
 
     loader = ptr.ExcelTableFileLoader(infname)
     writer = ptw.MarkdownTableWriter()
 
-    i = 0
-    try:
+    with open(outfname, "w") as f:
+        writer.stream = f
         for table_data in loader.load():
             writer.from_tabledata(table_data, is_overwrite_table_name = True)
-            writer.dump(outfname + "-" + str(i) + ".md")
-            i += 1
-    except:
-        status = "nok"
-
-    if status == "ok":
-        with open(outfname + ".md", 'w') as outfile:
-            for j in range(i):
-                with open(outfname + "-" + str(j) + ".md") as infile:
-                    outfile.write(infile.read())
-                os.remove(outfname + "-" + str(j) + ".md")
-    else:
-        for j in range(i):
-            os.remove(outfname + "-" + str(j) + ".md")
+            writer.write_table()
 
 
 if __name__ == "__main__":
