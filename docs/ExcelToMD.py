17f4ac81f0f12bfd6352c3d7b29a740449890446
stefanronn <114413089+stefanronn@users.noreply.github.com>
2023-01-20 08:07:20 +0100

    Fixed header levels and improved code

--- docs/ExcelToMD.py
+++ docs/ExcelToMD.py
@@ -5,10 +5,52 @@ import pytablewriter as ptw
 import sys
 
 
+def excel_to_md(infname, outfname):
+    loader = ptr.ExcelTableFileLoader(infname)
+    writer = ptw.MarkdownTableWriter()
+
+    with open(outfname, "w") as f:
+        f.write("# Hardware Discovery\n")
+        f.write("\n")
+        f.write("*{{ git_revision_date_localized }}*\n")
+        f.write("\n")
+        writer.stream = f
+        for table_data in loader.load():
+            writer.from_tabledata(table_data, is_overwrite_table_name = True)
+            writer.write_table()
+
+    return
+
+
+# Fix header levels
+def fix_headers(outfname):
+    with open(outfname, "r") as f:
+        linelist = f.read().splitlines()
+    
+    newlinelist = []
+    hlevel = 1
+    for line in linelist:
+        if len(line) > 0:
+            if line[0] == '#':
+                if hlevel == 1:
+                    hlevel = 2
+                else:
+                    line = '#' + line
+        newlinelist.append(line)
+
+    with open(outfname, "w") as f:
+        for line in newlinelist:
+            f.write(line + "\n")
+
+    return
+
+
+
+
 def main():
 
-    # infname = "C:/pythondev/ExPECA-lab-inventory-test.xlsx"  # Input Excel file name, full path
-    # outfname = "C:/pythondev/ExPECA-lab-inventory-test.md"      # Output markdown file name, full path
+    # infname = "A:/Documents/expeca/lab-inventory/ExPECA-HW-Discovery.xlsx"  # Input Excel file name, full path
+    # outfname = "A:/Documents/expeca/lab-inventory/ExPECA-HW-Discovery.md"      # Output markdown file name, full path
 
     # Command line input parameters
     # It has to be 2 command line parameters
@@ -18,18 +60,8 @@ def main():
     infname = sys.argv[1]                             # Input Excel file name, full path
     outfname = sys.argv[2]                            # Output markdown file name, full path
 
-    loader = ptr.ExcelTableFileLoader(infname)
-    writer = ptw.MarkdownTableWriter()
-
-    with open(outfname, "w") as f:
-        f.write("# Hardware discovery\n")
-        f.write("\n")
-        f.write("*{{ git_revision_date_localized }}*\n")
-        f.write("\n")
-        writer.stream = f
-        for table_data in loader.load():
-            writer.from_tabledata(table_data, is_overwrite_table_name = True)
-            writer.write_table()
+    excel_to_md(infname, outfname)
+    fix_headers(outfname)
 
     return
 
